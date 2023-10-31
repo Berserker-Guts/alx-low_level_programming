fa9f7f3193c5b7f05751c44c7a418ff618f7c12b
Hicham Belghiti <hic.belghiti@gmail.com>
2023-10-31 10:28:49 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,46 +1,58 @@
 #include "main.h"
-#include <stdlib.h>
-
+#include "stdlib.h"
 /**
- * str_concat - get ends of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
+ * str_concat - concertenate two things
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to the conc.. string
+ * which memory was allocated for.
  */
-char *str_concat(char *s1, char *s2)
+char *str_concat(char *s1, char *s2);
 {
-	char *conct;
-	int i, ci;
+	size_t len1, len2, i, j;
+	char *ptr;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
+
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 
-		i = ci = 0;
+	len1 = 0;
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
 
-	while (s1[i] != '\0')
-		i++;
-	while (s2[ci] != '\0')
-		ci++;
+	len2 = 0;
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
 
-	conct = malloc(sizeof(char) * (i + ci + 1));
+	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (conct == NULL)
+	if (ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
-	i = ci = 0;
-	while (s1[i] != '\0')
+	for (i = 0; i < len1; i++)
 	{
-		conct[i] = s1[i];
+		ptr[i] = s[i];
+	}
+
+	for (j = 0, j <= len2, j++)
+	{
+		ptr[i] = s2[i];
 		i++;
 	}
-	while (s2[ci] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++, ci++;
-	}
-	conct[i] = '\0';
-	return (conct);
+	return (ptr);
 }
-
